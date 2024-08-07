9776e3fecd7b4ca7abaa552f2d29f11fdd39b0b9
AtefehTaheri <e.atefehtaheri@gmail.com>
2024-08-07 15:56:11 +0330

    Add Items for Navigation Bar

---
+++ app/src/main/java/ir/atefehtaheri/movieapp/core/designsystem/icon/MovieIcon.kt
@@ -0,0 +1,19 @@
+package ir.atefehtaheri.movieapp.core.designsystem.icon
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
+
+object MovieIcons {
+    val Home = Icons.Filled.Home
+    val HomeBorder = Icons.Outlined.Home
+    val Search = Icons.Filled.Search
+    val SearchBorder = Icons.Outlined.Search
+    val Favorite = Icons.Filled.Favorite
+    val FavoriteBorder = Icons.Outlined.FavoriteBorder
+
+}
